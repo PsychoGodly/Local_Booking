31222fa0b0d8e3c78f8093a572a328332bc1c03d
PsychoGodly <90983397+PsychoGodly@users.noreply.github.com>
2024-05-21 15:47:20 +0100

    Redesigned Login page

--- Frontend/src/comp/Login.jsx
+++ Frontend/src/comp/Login.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import placeholderImage from "../assets/azura.png"; // Make sure to have a placeholder image in the same directory
 
 const Login = () => {
   const [credentials, setCredentials] = useState({ username: '', password: '' });
@@ -31,7 +32,9 @@ const Login = () => {
   return (
     <div className="min-h-screen flex items-center justify-center bg-gray-100">
       <div className="bg-white p-8 rounded-lg shadow-lg w-full max-w-md">
-        <h2 className="text-2xl font-bold text-center mb-6">Sign In</h2>
+        <div className="flex justify-center mb-6">
+          <img src={placeholderImage} alt="Logo" className="h-auto w-30 "/>
+        </div>
         <form onSubmit={handleLoginForm} className="space-y-6">
           <div>
             <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="username">
@@ -65,12 +68,16 @@ const Login = () => {
           </div>
           <button
             type="submit"
-            className="bg-[#FA323D] hover:bg-[#003D1E] text-white font-bold py-2 px-4 rounded w-full focus:outline-none focus:ring-2 focus:ring-blue-500"
+            className="bg-[#2f74b6] hover:bg-[#5375d3] text-white font-bold py-2 px-4 rounded w-full focus:outline-none focus:ring-2 focus:ring-blue-500"
           >
             Sign in
           </button>
         </form>
-        
+        <div className="text-center mt-4">
+          <a href="#" className="text-blue-500 hover:text-blue-700 text-sm">
+            Forgot your password?
+          </a>
+        </div>
       </div>
     </div>
   );
