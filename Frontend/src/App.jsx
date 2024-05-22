c85c02d8eb0bbef6b9119a6372e911ec5805c447
PsychoGodly <90983397+PsychoGodly@users.noreply.github.com>
2024-05-22 14:05:41 +0100

    Revert "Merge pull request #1 from MedDevp/frontend2"
    This reverts commit 7ebc671eac70a4ac26e40071fdd3db53041f61c6, reversing
    changes made to 6b9df259bbb1bda9d36404e9fa427985e12854c2.

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,13 +1,29 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Calendar from "./comp/Calendar";
+// import Calendar from "./component/Calendar";
+// import Test from "./component/Test";
 
+// import ReservationCalendar from "./component/ReservationCalendar";
+// import MyCalendar from "./component/MyCalendar";
 
 const App = () => {
   return (
     <div>
       <Router>
         <Switch>
+          {/* <Route path="/calendar">
+            <Calendar />
+          </Route> */}
+
+          {/* <Route path="/test">
+            <Test />
+          </Route>  */}
+
+          {/* <Route path="/test">
+            <ReservationCalendar />
+          </Route> */}
+
           <Route path="/test">
             <Calendar />
           </Route>
