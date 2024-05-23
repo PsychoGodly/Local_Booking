46e149981e234cbb8f9c7a3200ab574c3e2277de
PsychoGodly <90983397+PsychoGodly@users.noreply.github.com>
2024-05-23 23:27:38 +0100

    Enhanced Sidebar

--- Frontend/src/comp/SideBar.jsx
+++ Frontend/src/comp/SideBar.jsx
@@ -1,28 +1,39 @@
 import React from "react";
 import { Sidebar } from "flowbite-react";
 import { HiChartPie, HiShoppingBag, HiInbox, HiUser, HiCog, HiLogout, HiUsers, HiHome } from "react-icons/hi";
+import placeholderImage from "../assets/azura.png"; // Make sure to have a placeholder image in the same directory
 
 const SidebarComponent = () => {
   return (
-    <Sidebar aria-label="Sidebar with multi-level dropdown example">
-      <Sidebar.Items>
-        <Sidebar.ItemGroup>
-          <Sidebar.Item href="#" icon={HiChartPie}>
-            Reservations History
-          </Sidebar.Item>
-          <Sidebar.Collapse icon={HiUser} label="Administrator">
-            <Sidebar.Item href="#" icon={HiUsers}>Employees</Sidebar.Item>
-            <Sidebar.Item href="#" icon={HiHome}>Rooms</Sidebar.Item>
-          </Sidebar.Collapse>
-          <Sidebar.Item href="#" icon={HiCog}>
-            Settings
-          </Sidebar.Item>
-          <Sidebar.Item href="#" icon={HiLogout}>
-            Logout
-          </Sidebar.Item>
-        </Sidebar.ItemGroup>
-      </Sidebar.Items>
-    </Sidebar>
+    <div style={{ display: "flex", alignItems: "flex-end" }}>
+      {/* Sidebar Menu */}
+      <div style={{ flex: "0 0 auto", paddingTop: "20px" }}>
+        <Sidebar aria-label="Sidebar with multi-level dropdown example" style={{ textAlign: "left" }}>
+          <Sidebar.Items>
+            <Sidebar.ItemGroup>
+              <Sidebar.Item href="#" icon={HiChartPie}>
+                Reservations History
+              </Sidebar.Item>
+              <Sidebar.Collapse icon={HiUser} label="Administrator">
+                <Sidebar.Item href="#" icon={HiUsers}>Employees</Sidebar.Item>
+                <Sidebar.Item href="#" icon={HiHome}>Rooms</Sidebar.Item>
+              </Sidebar.Collapse>
+              <Sidebar.Item href="#" icon={HiCog}>
+                Settings
+              </Sidebar.Item>
+              <Sidebar.Item href="#" icon={HiLogout}>
+                Logout
+              </Sidebar.Item>
+            </Sidebar.ItemGroup>
+          </Sidebar.Items>
+        </Sidebar>
+      </div>
+
+      {/* Calendar */}
+      <div style={{ flex: "1 1 auto" }}>
+        {/* Render your Calendar component here */}
+      </div>
+    </div>
   );
 };
 
