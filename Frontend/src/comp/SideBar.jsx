8415c468b5278872220b4d366fdc6fdef8f27471
PsychoGodly <90983397+PsychoGodly@users.noreply.github.com>
2024-05-27 09:44:26 +0100

    SideBar rework

--- Frontend/src/comp/SideBar.jsx
+++ Frontend/src/comp/SideBar.jsx
@@ -1,37 +1,112 @@
-import React from "react";
-import { Sidebar } from "flowbite-react";
-import { HiChartPie, HiShoppingBag, HiInbox, HiUser, HiCog, HiLogout, HiUsers, HiHome } from "react-icons/hi";
-import placeholderImage from "../assets/azura.png"; // Make sure to have a placeholder image in the same directory
-
-const SidebarComponent = () => {
+import React, { useState } from 'react';
+import {
+  AppstoreOutlined,
+  ContainerOutlined,
+  DesktopOutlined,
+  MailOutlined,
+  MenuFoldOutlined,
+  MenuUnfoldOutlined,
+  PieChartOutlined,
+} from '@ant-design/icons';
+import { Button, Menu } from 'antd';
+const items = [
+  {
+    key: '1',
+    icon: <PieChartOutlined />,
+    label: 'Option 1',
+  },
+  {
+    key: '2',
+    icon: <DesktopOutlined />,
+    label: 'Option 2',
+  },
+  {
+    key: '3',
+    icon: <ContainerOutlined />,
+    label: 'Option 3',
+  },
+  {
+    key: 'sub1',
+    label: 'Navigation One',
+    icon: <MailOutlined />,
+    children: [
+      {
+        key: '5',
+        label: 'Option 5',
+      },
+      {
+        key: '6',
+        label: 'Option 6',
+      },
+      {
+        key: '7',
+        label: 'Option 7',
+      },
+      {
+        key: '8',
+        label: 'Option 8',
+      },
+    ],
+  },
+  {
+    key: 'sub2',
+    label: 'Navigation Two',
+    icon: <AppstoreOutlined />,
+    children: [
+      {
+        key: '9',
+        label: 'Option 9',
+      },
+      {
+        key: '10',
+        label: 'Option 10',
+      },
+      {
+        key: 'sub3',
+        label: 'Submenu',
+        children: [
+          {
+            key: '11',
+            label: 'Option 11',
+          },
+          {
+            key: '12',
+            label: 'Option 12',
+          },
+        ],
+      },
+    ],
+  },
+];
+const SideBar = () => {
+  const [collapsed, setCollapsed] = useState(false);
+  const toggleCollapsed = () => {
+    setCollapsed(!collapsed);
+  };
   return (
-    <div style={{ display: "flex", alignItems: "flex-end" }}>
-      {/* Sidebar Menu */}
-      <div style={{ flex: "0 0 auto", paddingTop: "20px"}}>
-        <Sidebar aria-label="Sidebar with multi-level dropdown example" style={{ textAlign: "left" }}>
-          <Sidebar.Items>
-            <Sidebar.ItemGroup>
-              <Sidebar.Item href="#" icon={HiChartPie}>
-                Reservations History
-              </Sidebar.Item>
-              <Sidebar.Collapse icon={HiUser} label="Administrator">
-                <Sidebar.Item href="#" icon={HiUsers}>Employees</Sidebar.Item>
-                <Sidebar.Item href="#" icon={HiHome}>Rooms</Sidebar.Item>
-              </Sidebar.Collapse>
-              <Sidebar.Item href="#" icon={HiCog}>
-                Settings
-              </Sidebar.Item>
-              <Sidebar.Item href="#" icon={HiLogout}>
-                Logout
-              </Sidebar.Item>
-            </Sidebar.ItemGroup>
-          </Sidebar.Items>
-        </Sidebar>
-      </div>
-
-      
+    <div
+      style={{
+        width: 256,
+      }}
+    >
+      <Button
+        type="primary"
+        onClick={toggleCollapsed}
+        style={{
+          marginBottom: 16,
+        }}
+      >
+        {collapsed ? <MenuUnfoldOutlined /> : <MenuFoldOutlined />}
+      </Button>
+      <Menu
+        defaultSelectedKeys={['1']}
+        defaultOpenKeys={['sub1']}
+        mode="inline"
+        theme="dark"
+        inlineCollapsed={collapsed}
+        items={items}
+      />
     </div>
   );
 };
-
-export default SidebarComponent;
+export default SideBar;
