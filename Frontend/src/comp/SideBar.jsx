fefc14423874c914bf21114778aa3c9ed9c42ae6
PsychoGodly <90983397+PsychoGodly@users.noreply.github.com>
2024-05-27 14:00:00 +0100

    Sidebar new icons + buttons are functional now

--- Frontend/src/comp/SideBar.jsx
+++ Frontend/src/comp/SideBar.jsx
@@ -1,37 +1,38 @@
-import React, { useState } from "react";
-import {
-  AppstoreOutlined,
-  ContainerOutlined,
-  DesktopOutlined,
-  MailOutlined,
-  MenuFoldOutlined,
-  MenuUnfoldOutlined,
-  PieChartOutlined,
-  LogoutOutlined,
-  UserAddOutlined,
-  PieChartFilled,
-  CalendarFilled,
-  CalendarOutlined
-} from "@ant-design/icons";
-import { Button, Menu } from "antd";
-import { icon } from "@fortawesome/fontawesome-svg-core";
+import React, { useState, useEffect } from "react";
+import { Menu } from "antd";
+import { useLocation } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faCalendar, faCalendarDays, faCircleUser, faHouse, faRightFromBracket, faScrewdriverWrench, faUser } from "@fortawesome/free-solid-svg-icons";
+import {
+  faCalendarAlt,
+  faCircleUser,
+  faHouse,
+  faRightFromBracket,
+  faScrewdriverWrench,
+  faUser
+} from "@fortawesome/free-solid-svg-icons";
+
+import { PieChartFilled } from "@ant-design/icons";
+
+const { SubMenu } = Menu;
+
 const items = [
   {
     key: "1",
-    icon: <PieChartFilled/>,
+    icon: <PieChartFilled />,
     label: "Dashboard",
+    link: "/dashboard"
   },
   {
     key: "2",
-    icon: <FontAwesomeIcon icon={faCalendarDays} />,
+    icon: <FontAwesomeIcon icon={faCalendarAlt} />,
     label: "Reservations",
+    link: "/calendar"
   },
   {
     key: "3",
     icon: <FontAwesomeIcon icon={faCircleUser} />,
     label: "Profile",
+    link: "/profile"
   },
   {
     key: "sub1",
@@ -40,51 +41,85 @@ const items = [
     children: [
       {
         key: "5",
-        icon:  <FontAwesomeIcon icon={faUser} />,
+        icon: <FontAwesomeIcon icon={faUser} />,
         label: "Users",
+        link: "/admin/users"
       },
       {
         key: "6",
-        icon:  <FontAwesomeIcon icon={faHouse} />,
+        icon: <FontAwesomeIcon icon={faHouse} />,
         label: "Rooms",
+        link: "/admin/rooms"
       },
       {
         key: "7",
-        icon:  <FontAwesomeIcon icon={faCalendar} />,
+        icon: <FontAwesomeIcon icon={faCalendarAlt} />,
         label: "Events",
+        link: "/admin/events"
       }
-    ],
+    ]
   },
   {
     key: "8",
-    icon:  <FontAwesomeIcon icon={faRightFromBracket} />,
+    icon: <FontAwesomeIcon icon={faRightFromBracket} />,
     label: "Logout",
+    link: "/logout"
   }
-  
 ];
+
 const SideBar = () => {
   const [collapsed, setCollapsed] = useState(false);
+  const [selectedKey, setSelectedKey] = useState("");
+  const location = useLocation();
+
+  useEffect(() => {
+    // Match location pathname with menu item link
+    const selectedItem = items.find(item => location.pathname.startsWith(item.link));
+    if (selectedItem) {
+      setSelectedKey(selectedItem.key);
+    }
+  }, [location]);
+
   const toggleCollapsed = () => {
     setCollapsed(!collapsed);
   };
+
+  const handleClick = (e) => {
+    setSelectedKey(e.key);
+  };
+
   return (
     <div
       style={{
-        width: 256,
+        width: 256
       }}
     >
-      
       <Menu
-        defaultSelectedKeys={["1"]}
-        defaultOpenKeys={["sub1"]}
         mode="inline"
         theme="dark"
         inlineCollapsed={collapsed}
-        items={items}
         className="h-[100vh]"
-
-      />
+        selectedKeys={[selectedKey]}
+        onClick={handleClick}
+      >
+        {items.map((item) =>
+          item.children ? (
+            <SubMenu key={item.key} icon={item.icon} title={item.label}>
+              {item.children.map((subItem) => (
+                <Menu.Item key={subItem.key} icon={subItem.icon}>
+                  <a href={subItem.link}>{subItem.label}</a>
+                </Menu.Item>
+              ))}
+            </SubMenu>
+          ) : (
+            <Menu.Item key={item.key} icon={item.icon}>
+              <a href={item.link}>{item.label}</a>
+            </Menu.Item>
+          )
+        )}
+      </Menu>
     </div>
   );
 };
+
 export default SideBar;
