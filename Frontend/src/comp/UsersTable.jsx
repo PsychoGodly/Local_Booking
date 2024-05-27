d648bef73f9331c2215e04757973d62b18d706e2
PsychoGodly <90983397+PsychoGodly@users.noreply.github.com>
2024-05-27 15:31:11 +0100

    User Adding form

--- Frontend/src/comp/UsersTable.jsx
+++ Frontend/src/comp/UsersTable.jsx
@@ -4,11 +4,8 @@ import config from "../Config";
 import Header from "./Header";
 import SideBar from "./SideBar";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {
-  faUserXmark,
-  faUserEdit,
-
-} from "@fortawesome/free-solid-svg-icons";
+import { faUserXmark, faUserEdit } from "@fortawesome/free-solid-svg-icons";
+import UserForm from "./UserForm";
 
 function UsersTable() {
   const [users, setUsers] = useState([]);
@@ -32,52 +29,62 @@ function UsersTable() {
         <div className=" -mt-8 mr-6">
           <SideBar />
         </div>
-        <div className="w-full">
-          <h2 className="text-xl font-bold mb-4">Users List</h2>
-          <div className="">
-            <table className=" border border-gray-400">
-              <thead>
-                <tr>
-                  <th className="px-4 py-2 bg-gray-200 border border-gray-400">
-                    ID
-                  </th>
-                  <th className="px-4 py-2 bg-gray-200 border border-gray-400">
-                    Username
-                  </th>
-                  <th className="px-4 py-2 bg-gray-200 border border-gray-400">
-                    Email
-                  </th>
-                  <th className="px-4 py-2 bg-gray-200 border border-gray-400">
-                    Role
-                  </th>
-                  <th className="px-4 py-2 bg-gray-200 border border-gray-400">
-                    Actions
-                  </th>
-                </tr>
-              </thead>
-              <tbody>
-                {users.map((user) => (
-                  <tr key={user.id}>
-                    <td className="px-4 py-2 border border-gray-400">
-                      {user.id}
-                    </td>
-                    <td className="px-4 py-2 border border-gray-400">
-                      {user.username}
-                    </td>
-                    <td className="px-4 py-2 border border-gray-400">
-                      {user.email}
-                    </td>
-                    <td className="px-4 py-2 border border-gray-400">
-                      {user.role}
-                    </td>
-                    <td className="px-4 py-2 border border-gray-400 flex space-x-2">
-                      <button className="bg-blue-500 text-white px-3 py-1 rounded hover:bg-blue-700"><FontAwesomeIcon icon={faUserEdit} /></button>
-                      <button className="bg-red-500 text-white px-3 py-1 rounded hover:bg-red-700"><FontAwesomeIcon icon={faUserXmark} /></button>
-                    </td>
+        <div className="flex justify-between items-center w-full">
+          <div className="w-full">
+            <h2 className="text-xl font-bold mb-4">Users List</h2>
+            <div className="">
+              <table className=" border border-gray-400">
+                <thead>
+                  <tr>
+                    <th className="px-4 py-2 bg-gray-200 border border-gray-400">
+                      ID
+                    </th>
+                    <th className="px-4 py-2 bg-gray-200 border border-gray-400">
+                      Username
+                    </th>
+                    <th className="px-4 py-2 bg-gray-200 border border-gray-400">
+                      Email
+                    </th>
+                    <th className="px-4 py-2 bg-gray-200 border border-gray-400">
+                      Role
+                    </th>
+                    <th className="px-4 py-2 bg-gray-200 border border-gray-400">
+                      Actions
+                    </th>
                   </tr>
-                ))}
-              </tbody>
-            </table>
+                </thead>
+                <tbody>
+                  {users.map((user) => (
+                    <tr key={user.id}>
+                      <td className="px-4 py-2 border border-gray-400">
+                        {user.id}
+                      </td>
+                      <td className="px-4 py-2 border border-gray-400">
+                        {user.username}
+                      </td>
+                      <td className="px-4 py-2 border border-gray-400">
+                        {user.email}
+                      </td>
+                      <td className="px-4 py-2 border border-gray-400">
+                        {user.role}
+                      </td>
+                      <td className="px-4 py-2 border border-gray-400 flex space-x-2">
+                        <button className="bg-blue-500 text-white px-3 py-1 rounded hover:bg-blue-700">
+                          <FontAwesomeIcon icon={faUserEdit} />
+                        </button>
+                        <button className="bg-red-500 text-white px-3 py-1 rounded hover:bg-red-700">
+                          <FontAwesomeIcon icon={faUserXmark} />
+                        </button>
+                      </td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+            </div>
+          </div>
+          <div>
+          <UserForm />
+
           </div>
         </div>
       </div>
