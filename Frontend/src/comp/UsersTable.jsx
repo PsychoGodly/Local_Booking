9455d7e5f3c3a234a4991e844c1c3658b81e136c
PsychoGodly <90983397+PsychoGodly@users.noreply.github.com>
2024-05-27 15:45:38 +0100

    Update UsersTable.jsx

--- Frontend/src/comp/UsersTable.jsx
+++ Frontend/src/comp/UsersTable.jsx
@@ -5,10 +5,7 @@ import Header from "./Header";
 import SideBar from "./SideBar";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faUserXmark, faUserEdit } from "@fortawesome/free-solid-svg-icons";
-<<<<<<< HEAD
-=======
 import UserForm from "./UserForm";
->>>>>>> Frontend
 
 function UsersTable() {
   const [users, setUsers] = useState([]);
@@ -44,68 +41,11 @@ function UsersTable() {
         <div className=" -mt-8 mr-6">
           <SideBar />
         </div>
-<<<<<<< HEAD
-        <div className="w-full">
-          <h2 className="text-xl font-bold mb-4">Users List</h2>
-          <div className="">
-            <table className="border border-gray-400">
-              <thead>
-                <tr>
-                  <th className="px-4 py-2 bg-gray-200 border border-gray-400">
-                    ID
-                  </th>
-                  <th className="px-4 py-2 bg-gray-200 border border-gray-400">
-                    Username
-                  </th>
-                  <th className="px-4 py-2 bg-gray-200 border border-gray-400">
-                    Email
-                  </th>
-                  <th className="px-4 py-2 bg-gray-200 border border-gray-400">
-                    Password
-                  </th>
-                  <th className="px-4 py-2 bg-gray-200 border border-gray-400">
-                    Role
-                  </th>
-                  <th className="px-4 py-2 bg-gray-200 border border-gray-400">
-                    Actions
-                  </th>
-                </tr>
-              </thead>
-              <tbody>
-                {users.map((user) => (
-                  <tr key={user.id}>
-                    <td className="px-4 py-2 border border-gray-400">
-                      {user.id}
-                    </td>
-                    <td className="px-4 py-2 border border-gray-400">
-                      {user.username}
-                    </td>
-                    <td className="px-4 py-2 border border-gray-400">
-                      {user.email}
-                    </td>
-                    <td className="px-4 py-2 border border-gray-400">
-                      {user.password}
-                    </td>
-                    <td className="px-4 py-2 border border-gray-400">
-                      {user.role}
-                    </td>
-                    <td className="px-4 py-2 border border-gray-400 flex space-x-2">
-                      <button className="bg-blue-500 text-white px-3 py-1 rounded hover:bg-blue-700">
-                        <FontAwesomeIcon icon={faUserEdit} />
-                      </button>
-                      <button
-                        className="bg-red-500 text-white px-3 py-1 rounded hover:bg-red-700"
-                        onClick={() => deleteUser(user.id)}
-                      >
-                        <FontAwesomeIcon icon={faUserXmark} />
-                      </button>
-                    </td>
-=======
         <div className="flex justify-between items-center w-full">
           <div className="w-full">
             <h2 className="text-xl font-bold mb-4">Users List</h2>
             <div className="">
-              <table className=" border border-gray-400">
+              <table className="border border-gray-400">
                 <thead>
                   <tr>
                     <th className="px-4 py-2 bg-gray-200 border border-gray-400">
@@ -123,7 +63,6 @@ function UsersTable() {
                     <th className="px-4 py-2 bg-gray-200 border border-gray-400">
                       Actions
                     </th>
->>>>>>> Frontend
                   </tr>
                 </thead>
                 <tbody>
@@ -145,7 +84,10 @@ function UsersTable() {
                         <button className="bg-blue-500 text-white px-3 py-1 rounded hover:bg-blue-700">
                           <FontAwesomeIcon icon={faUserEdit} />
                         </button>
-                        <button className="bg-red-500 text-white px-3 py-1 rounded hover:bg-red-700">
+                        <button
+                          className="bg-red-500 text-white px-3 py-1 rounded hover:bg-red-700"
+                          onClick={() => deleteUser(user.id)}
+                        >
                           <FontAwesomeIcon icon={faUserXmark} />
                         </button>
                       </td>
@@ -156,8 +98,7 @@ function UsersTable() {
             </div>
           </div>
           <div>
-          <UserForm />
-
+            <UserForm />
           </div>
         </div>
       </div>
