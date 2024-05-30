5084007905051873c062e020b80e4d6f2d75f5f7
MedDev <mohamedabail414@gmail.com>
2024-05-30 12:53:36 +0100

    add   ·addEvent· endpoint

--- Backend/src/main/java/org/example/backend/Controller/HolidayController.java
+++ Backend/src/main/java/org/example/backend/Controller/HolidayController.java
@@ -6,9 +6,9 @@ import org.example.backend.Repository.HolidayRepository;
 import org.example.backend.Service.ApiService;
 import org.example.backend.Service.HolidayService;
 import org.example.backend.model.Holiday;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -45,5 +45,17 @@ public class HolidayController {
     @GetMapping("/Num_event")
     public Long get_Users() {
         return holidayRepository.count();
+    }
+
+
+
+    @PostMapping("/addEvent")
+    public ResponseEntity<String> addEvent(@RequestBody Holiday event) {
+        try {
+            Holiday savedHoliday = holidayRepository.save(event);
+            return new ResponseEntity<>("Holiday added with ID: " + savedHoliday.getId(), HttpStatus.CREATED);
+        } catch (Exception e) {
+            return new ResponseEntity<>("Failed to add holiday", HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 }
