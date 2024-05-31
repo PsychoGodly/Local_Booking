f51ced047828c969f5a5935529a4a10018349d89
MedDev <mohamedabail414@gmail.com>
2024-05-31 11:14:07 +0100

    add taiwind to profile admin

--- Frontend/src/comp/UserProfile.jsx
+++ Frontend/src/comp/UserProfile.jsx
@@ -4,7 +4,7 @@ import UserImg from "../assets/user.png"; // Import your user image
 import config from "../Config";
 import Header from "./Header";
 import SideBar from "./SideBar";
-
+import back from "../assets/back.jpg";
 const UserProfile = () => {
   const [profileData, setProfileData] = useState(null);
   const [loading, setLoading] = useState(true);
@@ -26,58 +26,64 @@ const UserProfile = () => {
     fetchProfileData();
   }, []);
 
+  const handleEditProfile = () => {
+    // Implement logic for editing profile data
+    console.log("Editing profile...");
+  };
+
   return (
     <>
-      <div>
+      <div className="flex flex-col h-screen">
         <Header />
-        <div  className="flex">
+        <div className="flex flex-1">
           <SideBar />
-          <div>
-            <div className="max-w-lg mx-auto mt-8 bg-white p-8 rounded-lg ">
-              <h2 className="text-2xl font-bold mb-4">User Profile</h2>
+          <div className="flex-1">
+            <img
+                src={back}
+                alt="background"
+                className="w-full h-[150px]"
+                style={{opacity : "0.3"}}
+              />
+            <div className="max-w-lg -mt-36 bg-white p-8 ">
+              <img
+                src={UserImg}
+                alt="User Profile"
+                className="h-20 w-20 rounded-full"
+              />
               {loading ? (
                 <p>Loading...</p>
               ) : profileData ? (
-                <div className="flex items-center">
-                  <div className="mr-4">
-                    <img
-                      src={UserImg}
-                      alt="User Profile"
-                      className="h-20 w-20 rounded-full"
-                    />
-                  </div>
-                  <div>
-                    <div className="mb-4">
-                      <label className="block text-sm font-medium text-gray-700">
-                        Username:
-                      </label>
-                      <p className="mt-1 text-sm text-gray-900">
-                        {profileData.username}
-                      </p>
-                    </div>
-                    <div className="mb-4">
-                      <label className="block text-sm font-medium text-gray-700">
-                        Email:
-                      </label>
-                      <p className="mt-1 text-sm text-gray-900">
-                        {profileData.email}
-                      </p>
-                    </div>
-                    <div className="mb-4">
-                      <label className="block text-sm font-medium text-gray-700">
-                        Role:
-                      </label>
-                      <p className="mt-1 text-sm text-gray-900">
-                        {profileData.role}
-                      </p>
-                    </div>
-                    <div className="mb-4">
-                      <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">
-                        Change Photo
-                      </button>
-                    </div>
-                  </div>
-                </div>
+                <>
+                  <table className="w-full mt-24 p-6 shadow-sm">
+                    <tbody className="">
+                      <tr className="">
+                        <td className="pt-2 font-medium text-gray-700">Username:</td>
+                        <td className="text-gray-900">{profileData.username}</td>
+                      </tr>
+                      <tr className="">
+                        <td className="pt-2 font-medium text-gray-700">Email:</td>
+                        <td className="text-gray-900">{profileData.email}</td>
+                      </tr>
+                      <tr className="">
+                        <td className="pt-2 font-medium text-gray-700">Role:</td>
+                        <td className="text-gray-900">{profileData.role}</td>
+                      </tr>
+                      <tr className="">
+                        <td className="pt-2 font-medium text-gray-700">Country:</td>
+                        <td className="text-gray-900">FR</td>
+                      </tr>
+                      <tr className="">
+                        <td className="pt-2 font-medium text-gray-700">Phone:</td>
+                        <td className="text-gray-900">+23476346623247</td>
+                      </tr>
+                      <tr className="">
+                        <td className="pt-2 font-medium text-gray-700">Address:</td>
+                        <td className="text-gray-900">Street N.6</td>
+                      </tr>
+                    </tbody>
+                  </table>
+                  
+                </>
               ) : (
                 <p>No profile data found</p>
               )}
