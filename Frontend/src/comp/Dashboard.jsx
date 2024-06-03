a92599f3047efd8597d11d0062efc7735330d741
MedDev <mohamedabail414@gmail.com>
2024-06-03 08:39:25 +0100

    dashboard style

--- Frontend/src/comp/Dashboard.jsx
+++ Frontend/src/comp/Dashboard.jsx
@@ -47,36 +47,38 @@ const Dashboard = () => {
   return (
     <div>
       <Header />
-      <div className="calendar-layout flex   ">
+      <div className="calendar-layout flex ">
         {/*Sidbar com*/}
         <div className="sidebar">
           <SideBar />
         </div>
-        <div>
-          <div className="main-content w-full">
-            <div className="container mx-auto mt-8 px-4">
-              <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-8">
-                <div className="bg-blue-200 p-8 rounded-lg shadow-md">
-                  <h1 className="text-3xl font-bold mb-4">Users</h1>
-                  <h2 className="text-xl font-semibold">{numUsers}</h2>
-                </div>
-                <div className="bg-green-200 p-8 rounded-lg shadow-md">
-                  <h1 className="text-3xl font-bold mb-4">Rooms</h1>
-                  <h2 className="text-xl font-semibold">{numSalles}</h2>
-                </div>
-                <div className="bg-yellow-200 p-8 rounded-lg shadow-md">
-                  <h1 className="text-3xl font-bold mb-4">Reservations</h1>
-                  <h2 className="text-xl font-semibold">{numReservations}</h2>
-                </div>
-                <div className="bg-purple-200 p-8 rounded-lg shadow-md">
-                  <h1 className="text-3xl font-bold mb-4">Events</h1>
-                  <h2 className="text-xl font-semibold">{numEvents}</h2>
+        <div className="w-4/5 justify-center flex">
+          <div>
+            <div className="main-content w-full">
+              <div className="container mx-auto mt-8 px-4">
+                <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-8">
+                  <div className="bg-blue-200 p-8 rounded-lg shadow-md">
+                    <h1 className="text-3xl font-bold mb-4">Users</h1>
+                    <h2 className="text-xl font-semibold">{numUsers}</h2>
+                  </div>
+                  <div className="bg-green-200 p-8 rounded-lg shadow-md">
+                    <h1 className="text-3xl font-bold mb-4">Rooms</h1>
+                    <h2 className="text-xl font-semibold">{numSalles}</h2>
+                  </div>
+                  <div className="bg-yellow-200 p-8 rounded-lg shadow-md">
+                    <h1 className="text-3xl font-bold mb-4">Reservations</h1>
+                    <h2 className="text-xl font-semibold">{numReservations}</h2>
+                  </div>
+                  <div className="bg-purple-200 p-8 rounded-lg shadow-md">
+                    <h1 className="text-3xl font-bold mb-4">Events</h1>
+                    <h2 className="text-xl font-semibold">{numEvents}</h2>
+                  </div>
                 </div>
               </div>
             </div>
-          </div>
-          <div className="mt-6">
-            <ReservationStatistics />
+            <div className="mt-6">
+              <ReservationStatistics />
+            </div>
           </div>
         </div>
       </div>
